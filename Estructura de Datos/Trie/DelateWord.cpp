d1028c5a86b362c8dcf699948452c487d324978d
Brenda <101911555+Khenya@users.noreply.github.com>
2022-10-04 15:21:28 -0400

    Update DelateWord.cpp

--- Estructura de Datos/Trie/DelateWord.cpp
+++ Estructura de Datos/Trie/DelateWord.cpp
@@ -5,39 +5,33 @@ using namespace std;
 struct node {
     char currentCharacter;       
     bool isWord;
-    struct node *Cbefore;
-   //  int priority = 0;            
-    struct node *children[27];  // [null,null,null,......,null]
+    struct node *Cbefore;         
+    struct node *children[27];
     node() {
         isWord = false;
     }
 }*trie; 
-
-// Inicializar 
-
 void init() {
-    trie = new node();  // Instanciar el objeto trie
+    trie = new node();
 }
 
-void insertWord(string word) {   // alba 
+void insertWord(string word) { 
     node *currentNode =  trie;  
-    for (int i = 0; i< word.length(); i++) { // alba
-        int character = word[i] - 'a';       // i = 0 'a'-'a' = 0
+    for (int i = 0; i< word.length(); i++) {
+        int character = word[i] - 'a';      
         if(currentNode->children[character] == NULL ) {
             currentNode->children[character] = new node();
-           // currentNode->isWord = false;
         }
-      //   currentNode = max(currentNode->priority,priority);
         currentNode = currentNode->children[character];
         currentNode->currentCharacter = word[i];
     }
     currentNode->isWord = true;
 }
 
-bool searchWord(string word) {   // alto 
+bool searchWord(string word) { 
     node *currentNode =  trie;  
     for (int i = 0; i< word.length(); i++) {
-        int character = word[i] - 'a';       // i = 0 'a'-'a' = 0
+        int character = word[i] - 'a';
         if(currentNode->children[character] == NULL ) {
            return false;
         }
@@ -63,27 +57,6 @@ void deleteWord(string word){
     currentNode = NULL;
     delete currentNode;
 }
-/*
-int findWords(string prefix) {   // alto 
-    node *currentNode =  trie;  
-    for (int i = 0; i< prefix.length(); i++) {
-        int character = prefix[i] - 'a';       // i = 0 'a'-'a' = 0
-        if(currentNode->children[character] == NULL ) {
-           return 0;
-        }
-        currentNode = currentNode->children[character];
-    }
-    return currentNode->countWords;
-}*/
-/*
-void showTrie(node currentNode ) {
-    node *currentNode =  trie;  
-    for (int i =0; i< 27; i++) {
-        currentNode = currentCharacter = word[i];
-        cout (char(i+'a'));
-        showTrie(currentNode)
-    }
-}*/ 
 
 
 void isThereWord(string word) {
